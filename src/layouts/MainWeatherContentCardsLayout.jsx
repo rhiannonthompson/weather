8e50f5fa7fe7ae5e9687cc6307ae9ee938b259b2
Rhiannon Thompson <tommywoops@gmail.com>
2022-03-23 18:30:31 +0000

    fixed main heading

--- src/layouts/MainWeatherContentCardsLayout.jsx
+++ src/layouts/MainWeatherContentCardsLayout.jsx
@@ -13,7 +13,7 @@ export default function MainWeatherContentCardsLayout() {
   return (
     <>
       <div className="mt-12 mb-6">
-        <h2 className="text-text-white text-2xl font-light">Highlights</h2>
+        <h2 className="text-text-white text-2xl font-light">Today's Highlights</h2>
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-2 gap-5 pb-12">
