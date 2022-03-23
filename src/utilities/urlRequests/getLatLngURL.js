45b8ad3b2d8159420415170061dc7d50b791f7de
Rhiannon Thompson <tommywoops@gmail.com>
2022-03-23 14:25:33 +0000

    Fix netlify route

--- src/utilities/urlRequests/getLatLngURL.js
+++ src/utilities/urlRequests/getLatLngURL.js
@@ -1,7 +1,6 @@
 export default function getCordsURL(type, lat, lng) {
 
-  const URL = `/.netlify/functions/weather?type=${type}&lat=${lat}&lng=${lng}`;
-  // lambda function URL
+  const URL = `/functions/weather?type=${type}&lat=${lat}&lng=${lng}`;
 
   return URL
 }
