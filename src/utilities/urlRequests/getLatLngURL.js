cb7bb2b3f17e566032086e6c04003e62c5cbb6f7
Rhiannon Thompson <tommywoops@gmail.com>
2022-03-23 15:21:11 +0000

    Fix netlify route structure

--- src/utilities/urlRequests/getLatLngURL.js
+++ src/utilities/urlRequests/getLatLngURL.js
@@ -1,6 +1,6 @@
 export default function getCordsURL(type, lat, lng) {
 
-  const URL = `/netlify/functions/weather?type=${type}&lat=${lat}&lng=${lng}`;
+  const URL = `/.netlify/functions/weather?type=${type}&lat=${lat}&lng=${lng}`;
 
   return URL
 }
